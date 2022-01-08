212f4ccf8c08f566fde9034fabb89547151b85c4
fell <3227068950@qq.com>
2022-01-08 11:49:57 +0800

    new examples

---
+++ examples/linear_regression.py
@@ -0,0 +1,46 @@
+import trchime as tce
+
+def get_dataset(p_sum):
+    # randomly generate points
+    x_data = tce.random.randn(p_sum, 3)
+    # set linear problem's coefficient
+    coef = tce.Constant([[-1], [3], [-2]])
+
+    # construct standard module: y = kx + b
+    y_data = x_data @ coef + 5
+    # set noise for module
+    y_data += tce.random.randn(p_sum, 1)
+
+    return x_data, y_data
+
+class linearModel(tce.Module):
+    def __init__(self):
+        # declare coefficient variable w with initialize randomly
+        self.w = tce.random.randn(3, 1, requires_grad = True)
+        # declare bias with initialize randomly
+        self.b = tce.random.randn(requires_grad = True)
+
+    def predict(self, inputs):
+        # define your forward here
+        return inputs @ self.w + self.b
+
+# collect training data
+points_sum = 200
+x, y = get_dataset(points_sum)
+
+# instantiate your model
+model = linearModel()
+
+# compile your model
+model.compile(optimizer = tce.nn.SGD_OPTIMIZER,  # choose stochastic gradient descent optimizer
+              loss = tce.nn.MSELOSS,  # set mean square loss function
+              learning_rate = 0.01)  # set learning rate
+
+# train your model
+model.fit(x, y,  # input training data
+          batch_size = 32,  # set batch_size and epochs
+          epochs = 100)
+
+# show parameters:
+print(f"w: \n{model.w.data}\n")
+print(f"b: \n{model.b.data}")
---
+++ examples/minimize_func.py
@@ -0,0 +1,27 @@
+"""
+The idea here is that we'd like to use our library
+to minimize a function, say (x - c) ** 2
+"""
+import trchime as tce
+
+# declare variable x and constant c.
+x = tce.Variable([10., -10., -5., 6., 3., 1.])
+c = tce.Constant([1., 2., -7., 6., tce.pi, -tce.e])
+
+
+# minimize (x - c) ** 2 by 100 iterations
+for i in range(30):
+
+    y = (x - c) ** 2
+    z = y.sum()
+    z.backward()  # call backward method to compute x's gradient
+
+    # update x with method `assign_sub`
+    # specially, updating variable with `-=` operator would be inefficiently and slowly horribly
+    # we strongly recomend this way to update your network's parameters instead of `-=` operator
+    x.assign_sub(0.4 * x.grad.data)
+
+    print('z:', z.data)
+
+# show x's value
+print('\nvalue of x:\n', x.data)
