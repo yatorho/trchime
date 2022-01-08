068d1c439b5e28043e5551cdd17fd3ba0092959a
fell <3227068950@qq.com>
2022-01-08 12:50:53 +0800

    update mistake

--- examples/minimize_func.py
+++ examples/minimize_func.py
@@ -9,17 +9,18 @@ x = tce.Variable([10., -10., -5., 6., 3., 1.])
 c = tce.Constant([1., 2., -7., 6., tce.pi, -tce.e])
 
 
-# minimize (x - c) ** 2 by 30 iterations
-for i in range(30):
+# minimize (x - c) ** 2 by 100 iterations
+for i in range(100):
 
     y = (x - c) ** 2
     z = y.sum()
     z.backward()  # call backward method to compute x's gradient
 
+    delta = 0.1 * x.grad  # gradient descent delta
     # update x with method `assign_sub`
     # specially, updating variable with `-=` operator would be inefficiently and slowly horribly
     # we strongly recomend this way to update your network's parameters instead of `-=` operator
-    x.assign_sub(0.4 * x.grad.data)
+    x.assign_sub(delta)
 
     print('z:', z.data)
 
