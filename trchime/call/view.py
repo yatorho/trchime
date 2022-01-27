d4cc70a2eb7a79971637688d2dbe4309e4c37145
fell <3227068950@qq.com>
2022-01-27 16:40:25 +0800

    test

--- trchime/call/view.py
+++ trchime/call/view.py
@@ -12,11 +12,24 @@ class ProgressBar:
 
     def __call__(self, x):
         # x in percent
-        assert 100 >= x >= 0, "progressbar called argment should be in range(0, 101)"
+        assert 100 >= x >= 0, "progressbar called argument should be in range(0, 101)"
 
         self.pointer = int(self.width*(x/100.0))
 
         return "|" + "#"*self.pointer + "-"*(self.width-self.pointer)+ "| %d %% done" % int(x)
+
+class MessageBoard:
+    """
+
+    """
+
+    def __init__(self, height=30, width=50):
+        self.height = height
+        self.width = width
+
+    def addhorizontalline(self):
+        pass
+
 
 
 
